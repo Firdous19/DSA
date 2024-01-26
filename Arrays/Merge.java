66b52770111890e0afd5490ab531c0a810c1ae70
Firdous <firdousrahaman42@gmail.com>
2024-01-26 13:29:32 +0530

    To merge two Sorted array without extra space

--- Arrays/Merge.java
+++ Arrays/Merge.java
@@ -2,6 +2,8 @@ package Arrays;
 
 // Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
 
+//merging without extra space using gap method
+
 import java.util.Arrays;
 
 public class Merge {
@@ -39,7 +41,6 @@ public class Merge {
             if (gap == 1)
                 break;
             gap = (gap / 2) + (gap % 2);
-            System.out.println(gap);
         }
     }
 
