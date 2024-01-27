6dee3c8ba638ca3a2e024b4199ade7675ef5024b
Firdous <firdousrahaman42@gmail.com>
2024-01-27 16:06:16 +0530

    To Find the Lowerbound using Binary Search

---
+++ Binary_Search/LowerBound.java
@@ -0,0 +1,37 @@
+package Binary_Search;
+
+public class LowerBound {
+
+    int binarySearch(int arr[], int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int index = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= target) {
+                index = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return index;
+    }
+
+    int lowerBound(int arr[], int target) {
+        return binarySearch(arr, target);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 3, 5, 8, 15, 19, 19, 19 };
+        int target = 19;
+
+        LowerBound lower = new LowerBound();
+        int index = lower.lowerBound(arr, target);
+
+        System.out.println(index);
+    }
+}
