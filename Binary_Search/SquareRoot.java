71ab38ab67cd035cbe18dd8be77b65be27889a86
Firdous <firdousrahaman42@gmail.com>
2024-01-28 09:38:32 +0530

    To Find the Square Root of a given number

---
+++ Binary_Search/SquareRoot.java
@@ -0,0 +1,34 @@
+package Binary_Search;
+
+public class SquareRoot {
+
+    int squareRoot(int num) {
+        int low = 0;
+        int high = num;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int square = mid * mid;
+
+            if (square == num) {
+                return mid;
+            } else if (square > num) {
+                high = mid - 1;
+            } else {
+                index = mid;
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int num = 8;
+
+        SquareRoot root = new SquareRoot();
+        int sqrRoot = root.squareRoot(num);
+
+        System.out.println(sqrRoot);
+    }
+}
